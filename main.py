8b01662b3abd8a2190bfb20589f19ddfe6d03803
Pedro Stanzani <pedrohstanzani@gmail.com>
2023-03-26 18:06:54 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def read_image(filepath: str):
+    return cv2.imread(filepath)
+
+
+def display_image(image: np.ndarray):
+    plt.imshow(image, interpolation='none')
+    plt.show()
+
+
+def convert_color_space(image: np.ndarray, _from: str, _to: str):
+    from_to = {
+        'BGR': {
+          'RGB': cv2.COLOR_BGR2RGB
+        }
+    }
+
+    conversion_code = from_to[_from][_to]
+    return cv2.cvtColor(image, conversion_code)
+
+
+def transpose_image():
+    pass
+
+# def extract_red_green_blue(image: np.ndarray):
+#     """
+#     Usage: red_channel, green_channel, blue_channel = extract_red_green_blue(image)
+#     """
+#     return cv2.split(image)
